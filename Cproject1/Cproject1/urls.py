2ff6808ca16c41ac768ee707fde80aa72be8d52e
vijaykjeughale <vijaykj65@gmail.com>
2025-04-06 21:00:16 +0530

    [ADD] requirements.txt content

--- Cproject1/Cproject1/urls.py
+++ Cproject1/Cproject1/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ca/', include('crudApp1.urls'))
+    path('', include('crudApp1.urls'))
 ]
--- Cproject1/crudApp1/urls.py
+++ Cproject1/crudApp1/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path('vv/', views.votersView, name='add_url'),
-    path('dv/', views.displayInfo, name='show_url'),
+    path('/', views.displayInfo, name='show_url'),
     path('uv/<int:pk>', views.updateInfo, name='upd_url'),
     path('dv/<int:pk>', views.deleteInfo, name='del_url')
 ]
